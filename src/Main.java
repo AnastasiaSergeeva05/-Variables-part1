f6aa8272fa94b6a9c2c93b9f41a3027b9100b384
02sergey90 <82273253+02sergey90@users.noreply.github.com>
2022-09-08 11:29:20 +0300

    Сделано домашнее задание № 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашнее задание №1
+
+
+
+            // Задача № 1
+            var dog = 8;
+            System.out.println(dog);
+
+            var cat = 3.6;
+            System.out.println(cat);
+
+            var paper = 763789;
+            System.out.println(paper);
+            // Задача № 2
+            dog = dog + 4;
+            System.out.println(dog);
+
+            cat = cat + 4;
+            System.out.println(cat);
+
+            paper = paper + 4;
+            System.out.println(paper);
+            //Задача № 3
+            var dog1 = 8.0;
+            dog1 = dog1 - 3.5;
+            System.out.println(dog);
+
+            cat = cat - 1.6;
+            System.out.println(cat);
+
+            paper = paper - 7639;
+            System.out.println(paper);
+
+        }
     }
-}
+
