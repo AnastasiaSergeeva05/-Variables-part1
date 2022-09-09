7b21a44248f47120bafcdfcd7f6f88c216b3f999
AnastasiaSergeeva05 <112942778+AnastasiaSergeeva05@users.noreply.github.com>
2022-09-09 18:29:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        // Задание 1
+        //  int age = 18;
+      //  if (age >=18){
+
+       // System.out.println("возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+   // }
+
+
+                // первая задача
+                var dog = 8;
+                System.out.println(dog);
+                var cat = 3.6;
+                System.out.println(cat);
+                var paper = 763789;
+                System.out.println(paper);
+                // вторая задача
+                dog = dog + 4;
+                System.out.println(dog);
+                cat = cat + 4;
+                System.out.println(cat);
+                paper = paper + 4;
+                System.out.println(paper);
+                //третья задача
+                var dog1 = 8.0;
+                dog1 = dog1 - 3.5;
+                System.out.println(dog);
+                cat = cat - 1.6;
+                System.out.println(cat);
+                paper = paper - 7639;
+                System.out.println(paper);
+        // задача 1
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //задача 2
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        // Домашнее задание 3
+
+                var firstBoxer = 72.2;
+                var secondBoxer = 82.7;
+                var totalWeight = firstBoxer + secondBoxer;
+                System.out.println(totalWeight);
+                var difference = secondBoxer - firstBoxer;
+                System.out.println(difference);
+        // Домашнее задание 3
+
+                var firstBoxer1 = 72.2;
+                var secondBoxer1 = 82.7;
+                var totalWeight1 = firstBoxer1 + secondBoxer1;
+                System.out.println(totalWeight1);  //задание 6
+                var difference1 = secondBoxer1 - firstBoxer1;
+                System.out.println(difference1);   //задание 6
+        // Домашнее задание 3
+
+                var firstBoxer2 = 72.2;
+                var secondBoxer2 = 82.7;
+                var difference2 = secondBoxer2 - firstBoxer2;
+                System.out.println(difference2);  // задание 7
+                var remainder = secondBoxer2 % firstBoxer2; // задание 7
+                System.out.println(remainder);
+        //Домашнее задание 3
+      
+                // Задание 8
+                var totalHours = 640;
+                var oneEmployee = 8;
+                var totalEmployoee = totalHours / oneEmployee;
+                System.out.println("\"Всего работников в компании –" + " " + totalEmployoee + " "+ "человек.\" ");
+}
 }
